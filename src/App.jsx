f80f78c1cd52f74d6bf2f5213f1bb54c089f3197
Deva9754 <deva.sahu2001@gmail.com>
2025-03-06 13:24:28 +0530

    fix : error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 // App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
 import ArticleDetails from "./components/ArticleDetails";
 import Footer from "./components/Footer";
+import Dashboard from "./components/Dashboard";
 
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
     <Router>
      
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Dashboard />} />
         <Route path="/article/:title" element={<ArticleDetails/>} />
       </Routes>
       <Footer/>
