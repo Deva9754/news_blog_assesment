e21b5e4800433f0954aab1586c2b77d313113bb4
Deva9754 <deva.sahu2001@gmail.com>
2025-03-06 13:31:21 +0530

    dashboard issue fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 // App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Home from "./pages/Home";
+ import Home from "./pages/Home";
 import ArticleDetails from "./components/ArticleDetails";
 import Footer from "./components/Footer";
-import Dashboard from "./components/Dashboard";
 
 
 function App() {
@@ -13,7 +12,7 @@ function App() {
     <Router>
      
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<Home />} />
         <Route path="/article/:title" element={<ArticleDetails/>} />
       </Routes>
       <Footer/>
